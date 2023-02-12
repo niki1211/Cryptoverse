e49e9ae4cb0682a42f1085f86a0481645208bb5d
Sandeep Karjala <sandeepkarjala@Sandeeps-MacBook-Pro.local>
2023-02-12 13:02:30 -0500

    CryptoCrr changes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { millify } from 'millify';
+import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
@@ -9,11 +9,14 @@ const { Title } = Typography;
 
 const Homepage = () =>
 {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   console.log( data );
   
-  if ( isFetching ) return 'Loading...';
   const globalStats = data?.data?.stats;
+  console.log( globalStats )
+  
+  if ( isFetching ) return 'Loading...';
+  
   return (
     <>
       <Title level={ 2 } className="heading">Global Crypto Stats</Title>
@@ -28,7 +31,7 @@ const Homepage = () =>
         <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
         <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
       </div>
-      <Cryptocurrencies simplified/>
+      <Cryptocurrencies simplified />
       <div className="home-heading-container">
         <Title level={2} className="home-title">Latest Crypto News</Title>
         <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
